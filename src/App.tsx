91e6e5d8175a8388e97e50e4482bf0a2a0f3f9af
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2023-10-03 12:11:17 +0200

    functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component} from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/* Componente funzionale */
+/* function App(){
+  return <h1>Hello World</h1>
+} */
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Davide + Clara</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+/* Componente di classe */
+/* class App extends Component{
+  render() {
+    return <h1>Hello World</h1> 
+}} */
 
-export default App
+/* Componente funzionale ES6 */
+const App = () => <h1>Hello World</h1>
+
+export default App;
